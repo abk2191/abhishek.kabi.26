d0e8ae09b571eaca5ea7a5e75f2bc84ec7fc909f
Abhishek <abhishek.kabi.21@gmail.com>
2026-04-28 00:55:12 +0530

    New Update

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/abhishekkabi/",
+  base: "/abhishek.kabi.26/",
 });
